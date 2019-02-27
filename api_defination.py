b67f69c05ab98dbfecb268603eaf54ae44e6251f
aram2726 <aramharoyan@gmail.com>
2019-02-27 20:43:59 +0400

    updates

--- api_defination.py
+++ api_defination.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 
 
 api = Api(
-    app, version='1.0', title='Blockchain API',
+    app,
+    version='1.0',
+    title='Blockchain API',
     description='Simple API based on Blockchain technology',
+    contact='email aramharoyan@gmail.com',
+    contact_email='aramharoyan@gmail.com',
+    license='License MIT',
+    license_url='https://github.com/aram2726/blockchain_api/blob/master/LICENSE',
 )
--- utils.py
+++ utils.py
@@ -3,9 +3,11 @@ import hashlib
 
 BLOCKCHAIN_DIR = os.path.join(os.path.dirname(__file__), "blockchain/")
 
+
 def get_blockchain_files():
     files = os.listdir(BLOCKCHAIN_DIR)
     return sorted([int(i) for i in files])
+
 
 def get_blockchain_file(name):
     return os.path.join(BLOCKCHAIN_DIR, str(name)) or None
